8169fd63b77d9b3759a84487f37608ef486c96f9
Ohgyun Ahn <ohgyun@gmail.com>
2016-11-03 23:59:11 +0900

    Run notebook container with name

--- run_notebook.sh
+++ run_notebook.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-docker run -it -p 9999:8888 --name tensorflow_notebook -v $(pwd)/notebooks:/notebooks gcr.io/tensorflow/tensorflow
-open http://localhost:9999
+container_name=tensorflow_notebook
+image_repository=gcr.io/tensorflow/tensorflow
+host_port=9999
+
+if [[ -n $(docker ps -q -f name=${container_name}) ]]; then
+    echo "Container is already running"
+elif [[ -n $(docker ps -aq -f name=${container_name}) ]]; then
+    echo "Restart container"
+    docker restart $container_name
+else
+    echo "Create and run new container"
+    docker run -d -p $host_port:8888 --name $container_name -v $(pwd)/notebooks:/notebooks $image_repository
+fi
+
+sleep 1
+open http://localhost:$host_port
